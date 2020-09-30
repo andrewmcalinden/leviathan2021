478ddcabec4fe4f00ce5d8082c1973324b459c15
droiddoes9 <52133386+droiddoes9@users.noreply.github.com>
2020-09-29 19:33:27 -0500

    actual pixel vals, made bitmap instance

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vision.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vision.java
@@ -18,7 +18,11 @@ public class Vision {
     private LinearOpMode opMode;
     private VuforiaLocalizer vuforia;
 
-    public Vision(LinearOpMode opMode) {
+    private Bitmap bitmap;
+
+    //private final static double WIDTH_CONVERSION_FACTOR;
+
+    public Vision(LinearOpMode opMode) throws InterruptedException{
         this.opMode = opMode;
         VuforiaLocalizer.Parameters params = new VuforiaLocalizer.Parameters();
         params.vuforiaLicenseKey = "AQvLCbX/////AAABmTGnnsC2rUXvp1TAuiOSac0ZMvc3GKI93tFoRn4jPzB3uSMiwj75PNfUU6MaVsNZWczJYOep8LvDeM/3hf1+zO/3w31n1qJTtB2VHle8+MHWNVbNzXKLqfGSdvXK/wYAanXG2PBSKpgO1Fv5Yg27eZfIR7QOh7+J1zT1iKW/VmlsVSSaAzUSzYpfLufQDdE2wWQYrs8ObLq2kC37CeUlJ786gywyHts3Mv12fWCSdTH5oclkaEXsVC/8LxD1m+gpbRc2KC0BXnlwqwA2VqPSFU91vD8eCcD6t2WDbn0oJas31PcooBYWM6UgGm9I2plWazlIok72QG/kOYDh4yXOT4YXp1eYh864e8B7mhM3VclQ";
@@ -26,9 +30,10 @@ public class Vision {
         vuforia = ClassFactory.getInstance().createVuforia(params);
         Vuforia.setFrameFormat(PIXEL_FORMAT.RGB565, true); //enables RGB565 format for the image
         vuforia.setFrameQueueCapacity(4); //tells VuforiaLocalizer to only store one frame at a time
+        getBitmap();
     }
 
-    public Bitmap getBitmap() throws InterruptedException {
+    public void getBitmap() throws InterruptedException {
         VuforiaLocalizer.CloseableFrame frame = vuforia.getFrameQueue().take();
         long numImages = frame.getNumImages();
         Image rgb = null;
@@ -42,16 +47,20 @@ public class Vision {
         }
         Bitmap bm = Bitmap.createBitmap(rgb.getWidth(), rgb.getHeight(), Bitmap.Config.RGB_565);
         bm.copyPixelsFromBuffer(rgb.getPixels());
-        return bm;
+
+        opMode.telemetry.addData("width ", rgb.getWidth());
+        opMode.telemetry.addData("height ", rgb.getHeight());
+        opMode.telemetry.update();
+
+        bitmap = bm;
     }
 
     //yellow RGB value is 255, 255, 0
     //checks 1 row if pixels that should be in the area where the top ring is
     //checks 80 pixels, and if 40 are yellow, it returns true
     public boolean yellowInTop() throws InterruptedException {
-        final int topY = 325;
-        final int topXLeft = 600, topXRight = 680;
-        Bitmap bitmap = getBitmap();
+        final int topY = 365;
+        final int topXLeft = 380, topXRight = 610;
         int numYellow = 0;
 
         for(int x = topXLeft; x < topXRight; x++){
@@ -66,9 +75,8 @@ public class Vision {
     //checks 1 row if pixels that should be in the area where the bottom ring is
     //checks 80 pixels, and if 40 are yellow, it returns true
     public boolean yellowInBottom() throws InterruptedException {
-        final int bottomY = 425;
-        final int bottomXLeft = 600, bottomXRight = 680;
-        Bitmap bitmap = getBitmap();
+        final int bottomY = 430;
+        final int bottomXLeft = 380, bottomXRight = 610;
         int numYellow = 0;
 
         for(int x = bottomXLeft; x < bottomXRight; x++){
