2f7d5292f42bc83b78ae7e3b54dfeab656046259
rohitalamgari <rohit.alamgari@gmail.com>
2020-11-28 16:57:07 -0600

    Test Commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/AdvancedTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/AdvancedTeleop.java
@@ -4,6 +4,7 @@ public class AdvancedTeleop extends AdvancedLib {
 
     @Override
     public void loop() {
+        //Update to field centric
         robotCentricTrigMecanum(gamepad1.left_stick_x, gamepad1.left_stick_y, gamepad1.right_stick_x);
         updateIntake();
         updateShooter();
