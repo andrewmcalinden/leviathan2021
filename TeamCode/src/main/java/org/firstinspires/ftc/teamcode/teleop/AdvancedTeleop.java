e22631953eb5bb4877250edbbcf6fab40a43db88
droiddoes9 <52133386+droiddoes9@users.noreply.github.com>
2020-12-08 17:30:36 -0600

    better comments

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/AdvancedTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/AdvancedTeleop.java
@@ -7,9 +7,9 @@ public class AdvancedTeleop extends AdvancedLib {
         double robotHeadingRad = gyro.getAngle() * (Math.PI / 180);
         //might need to negate left stick y because apparently that's a thing
         fieldCentricMecanum(gamepad1.left_stick_x, -gamepad1.left_stick_y, gamepad1.right_stick_x, robotHeadingRad);
-        updateIntake(); //hold right bumper
-        updateShooter(); //toggle a and b
-        updateGrabber(); //gamepad2 right stick y
-        updateTransfer(); //hold y, tap x for servo
+        updateIntake(); //hold right bumper gamepad1
+        updateShooter(); //toggle a and b gamepad1
+        updateGrabber(); //right stick y gamepad2
+        updateTransfer(); //hold y, tap x for servo gamepad1
     }
 }
