efd5866ec2cad16d91392c80f2ddfbea1a854db9
droiddoes9 <52133386+droiddoes9@users.noreply.github.com>
2021-01-19 19:47:18 -0600

    flip strafing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/AdvancedTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/AdvancedTeleop.java
@@ -9,7 +9,7 @@ public class AdvancedTeleop extends AdvancedLib {
     public void loop() {
         double robotHeadingRad = gyro.getAngle() * (Math.PI / 180);
         //might need to negate left stick y because apparently that's a thing
-        fieldCentricMecanum(-gamepad1.left_stick_x, -gamepad1.left_stick_y, gamepad1.right_stick_x, robotHeadingRad);
+        fieldCentricMecanum(gamepad1.left_stick_x, -gamepad1.left_stick_y, gamepad1.right_stick_x, robotHeadingRad);
         //updateIntake(); //hold right bumper gamepad1
         updateShooter(); //toggle a and b gamepad1
 //        updateGrabber(); //right stick y gamepad2
